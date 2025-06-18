53ae6b2fddf199379782088fc2e246ab40d3d50c
Dhairya2755 <gandhidhairya510@gmail.com>
2025-06-18 11:17:20 +0530

    register

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path 
 from .views import *
 urlpatterns = [
-    path('',index_inner,name='dashboard/index_inner.html'),
-    # path('outer/',index_outer,name='index_outer.html'),
+    path('',register,name="dashboard/register.html"),
+    path('inner/',index_inner,name='dashboard/index_inner.html'),
 ]
+    # path('outer/',index_outer,name='index_outer.html'),
+
--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 def index_inner(request):
     return render(request,'dashboard/index_inner.html')
 
 # def index_outer(request):
 #     return render(request,'index_outer.html')
+
+def register(request):
+    if request.method == "POST":
+        fullname = request.POST.get("fullname")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        confirm_password = request.POST.get("confirm_password")
+        return HttpResponse(f"Registered: {fullname} with email {email}")
+    
+    return render(request, 'dashboard/register.html')
